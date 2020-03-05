f4b61e097324189f0fe0a7f1fe865047f651a218
hyunW3 <0176778602@naver.com>
2020-03-06 01:35:57 +0900

    project0

--- init.c
+++ init.c
@@ -21,6 +21,9 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1,"Student ID: 2016310932\n");
+    printf(1,"Name: HyunWoong Bae\n");
+    printf(1,"========Any Message=======\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
